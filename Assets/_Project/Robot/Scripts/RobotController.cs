b12311751f9d6d9558fe746e2083e14d1ce61343
Alex Pontes <alex.bileck@gmail.com>
2025-02-26 21:02:36 -0300

    Update robot controller

--- Assets/_Project/Robot/Scripts/RobotController.cs
+++ Assets/_Project/Robot/Scripts/RobotController.cs
@@ -1,32 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class RobotController : MonoBehaviour
 {
-    public WheelCollider[] leftWheel;
-    public WheelCollider[] rightWheel;
+    public float speed = 3f;
+    public float rotationSpeed = 100f;
+    public float gravityMultiplier = 50f;
+    public float groundStickForce = 10f;
     public Transform centerOfMass;
+    public float groundCheckDistance = 0.2f; // Distância do chão para considerar que está aterrissado
+    public LayerMask groundLayer; // Camada do chão
 
-    public float motorForce = 1500f;
-    public float brakeForce = 3000f;
-    public float downforce = 500f;
-
-    private float motorInput;
-    private float steerInput;
-    private bool isTurning;
-
-    private Vector2 moveMag;
     private Vector2 move;
     private Rigidbody rb;
+    private bool isGrounded;
 
     public void GetMoveInput(InputAction.CallbackContext callbackContext)
     {
         move = callbackContext.ReadValue<Vector2>();
-        steerInput = move.x;
-        motorInput = move.y;
     }
 
     private void Awake()
@@ -36,103 +27,33 @@ public class RobotController : MonoBehaviour
 
     private void Start()
     {
-        if(centerOfMass != null)
-            rb.centerOfMass = centerOfMass.position;
+        rb.centerOfMass = centerOfMass.localPosition;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        //motorInput = Input.GetAxis("Vertical");
-        //steerInput = Input.GetAxis("Horizontal");
+        CheckGrounded();
 
-        if (Mathf.Abs(steerInput) > 0.1f && !isTurning)
+        rb.AddForce(Physics.gravity * gravityMultiplier, ForceMode.Acceleration);
+
+        rb.AddForce(-transform.up * groundStickForce, ForceMode.Acceleration);
+
+        if (!isGrounded)
         {
-            ApplyBraking();
-            isTurning = true;
-        }
-        else if (Mathf.Abs(steerInput) <= 0.1f)
-        {
-            isTurning = false;
+            return;
         }
 
-        if (!isTurning)
-        {
-            moveMag = new Vector2(motorInput, steerInput).normalized;
-            if (moveMag.magnitude > 0.1f)
-            {
-                HandleMotor();
-            }
-            else
-            {
-                ApplyBraking();
-            }
-        }
-        else
-        {
-            HandleTurning();
-        }
+        Vector3 moveDirection = transform.forward * move.y * speed;
+        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y * 0.1f, moveDirection.z);
 
+        // Rotação do robô
+        float rotation = move.x * rotationSpeed * Time.fixedDeltaTime;
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0, rotation, 0));
     }
 
-    private void HandleMotor()
+    private void CheckGrounded()
     {
-        for (int i = 0; i < leftWheel.Length; i++)
-        {
-            leftWheel[i].brakeTorque = 0;
-        }
-        for (int i = 0; i < rightWheel.Length; i++)
-        {
-            rightWheel[i].brakeTorque = 0;
-        }
-
-        float leftMotor = motorInput * motorForce;
-        float rightMotor = motorInput * motorForce;
-
-        for (int i = 0; i < leftWheel.Length; i++)
-        {
-            leftWheel[i].motorTorque = leftMotor;
-        }
-        for (int i = 0; i < rightWheel.Length; i++)
-        {
-            rightWheel[i].motorTorque = rightMotor;
-        }
-    }
-
-    private void ApplyBraking()
-    {
-        for (int i = 0; i < leftWheel.Length; i++)
-        {
-            leftWheel[i].brakeTorque = brakeForce;
-            leftWheel[i].motorTorque = 0;
-        }
-        for (int i = 0; i < rightWheel.Length; i++)
-        {
-            rightWheel[i].brakeTorque = brakeForce;
-            rightWheel[i].motorTorque = 0;
-        }
-    }
-
-    private void HandleTurning()
-    {
-        for (int i = 0; i < leftWheel.Length; i++)
-        {
-            leftWheel[i].brakeTorque = 0;
-        }
-        for (int i = 0; i < rightWheel.Length; i++)
-        {
-            rightWheel[i].brakeTorque = 0;
-        }
-
-        float leftMotor = steerInput * motorForce;
-        float rightMotor = -steerInput * motorForce;
-
-        for (int i = 0; i < leftWheel.Length; i++)
-        {
-            leftWheel[i].motorTorque = leftMotor;
-        }
-        for (int i = 0; i < rightWheel.Length; i++)
-        {
-            rightWheel[i].motorTorque = rightMotor;
-        }
+        // Raycast do centro do robô para baixo, verificando o chão
+        isGrounded = Physics.Raycast(transform.position, -transform.up, groundCheckDistance, groundLayer);
     }
 }
